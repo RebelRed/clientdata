46751c648a58dfcc2be1ef30b72ee19c7afabef6
RebelRed <rgdemstar@live.com>
2016-08-24 19:48:12 +0530

    boostrap design modification

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,7 +2,7 @@ class Entity < ActiveRecord::Base
 has_many :addresses 
 accepts_nested_attributes_for :addresses, allow_destroy: true
 
-  self.per_page = 10
+  self.per_page = 3
 
 def self.search(search)
 	where("entity_name LIKE :search OR existing_type LIKE :search OR credit LIKE :search ", search: "%#{search}%")
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,6 @@
 class Person < ActiveRecord::Base
 
-  self.per_page = 10
+  self.per_page = 3
 
 def self.search(search)
 	where('first_name LIKE :search OR email LIKE :search OR tags LIKE :search', search: "%#{search}%") 
